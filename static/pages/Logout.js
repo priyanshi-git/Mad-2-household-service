0f788d1e7eea09049c55c9ee78575f51e9a75d9c
Priyanshi Goel <priyanshigoel2010@gmail.com>
2024-08-09 23:49:12 +0530

    frontend initiated - created 1 dashboard

--- static/pages/Logout.js
+++ static/pages/Logout.js
@@ -1,7 +1,25 @@
 const Logout = {
   template: `
-  <h1>This is Logout</h1>
-  `
+    <div> 
+        <h1 v-if="logoutSuccess">Successful Logged out </h1>
+        <h1 v-else> Logout Unsuccessful</h1>
+    </div>
+    `,
+  data() {
+    return {
+      logoutSuccess: false,
+    };
+  },
+  methods: {
+    async created() {
+      console.log("Logout created lifecycle hit!! ");
+
+      res = await fetch(window.location.origin + "/logout");
+      if (res.ok) {
+        this.logoutSuccess = True;
+      }
+    },
+  },
 };
 
 export default Logout;
