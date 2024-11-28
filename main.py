214ed6dcb55f711b9892f4fc57e13b61d536bfef
Priyanshi Goel <priyanshigoel2010@gmail.com>
2024-11-28 20:35:37 +0530

    celery work again

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from config import DevelopmentConfig
 from application.resources import api
 from flask_security import SQLAlchemyUserDatastore, Security
 from application.sec import datastore
+from application.worker import celery_init_app
 
 def create_app():
   app = Flask(__name__)
@@ -18,6 +19,7 @@ def create_app():
   return app
 
 app = create_app()
+celery_app = celery_init_app(app)
 
 if __name__ == "__main__":
   app.run(debug=True)
